b8eed258f0b6938429d319f95e947cd874225a81
Trần Đức Lương A3 <tranducluong582@gmail.com>
2025-10-04 14:21:20 +0700

    Restore all project files including Identity Service and add .gitignore

---
+++ Identity Service/Models/DTOs.cs
@@ -0,0 +1,17 @@
+﻿namespace Identity_Service.Models.DTO
+{
+    public class RegisterDto
+    {
+        public string UserId { get; set; } = null!;
+        public string HoTen { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? SoDienThoai { get; set; }
+        public string MatKhau { get; set; } = null!;
+    }
+
+    public class LoginDto
+    {
+        public string UserId { get; set; } = null!;
+        public string MatKhau { get; set; } = null!;
+    }
+}
---
+++ Identity Service/Models/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Identity_Service.Models;
+
+public partial class Staff
+{
+    public string MaNv { get; set; } = null!;
+
+    public string HoTen { get; set; } = null!;
+
+    public string? ChucVu { get; set; }
+
+    public DateOnly? NgaySinh { get; set; }
+
+    public string? SoDienThoai { get; set; }
+}
