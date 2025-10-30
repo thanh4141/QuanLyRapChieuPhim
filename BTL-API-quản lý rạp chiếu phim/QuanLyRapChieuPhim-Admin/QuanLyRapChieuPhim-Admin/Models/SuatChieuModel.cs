27f5b170925d979aecfb344e047b15668e1b520a
dung444 <dungzp23@gmail.com>
2025-10-30 10:13:32 +0700

    Add files via upload

---
+++ BTL-API-quản lý rạp chiếu phim/QuanLyRapChieuPhim-Admin/QuanLyRapChieuPhim-Admin/Models/SuatChieuModel.cs
@@ -0,0 +1,20 @@
+﻿namespace QuanLyRapChieuPhim_Admin.Models
+{
+    public class SuatChieuModel
+    {
+        public int Id { get; set; }
+        public int PhimId { get; set; }
+        public int PhòngChiếuId { get; set; }
+        public DateTime NgàyChiếu { get; set; }
+        public TimeSpan GiờBắtĐầu { get; set; }
+        public TimeSpan GiờKếtThúc { get; set; }
+        public decimal GiáCơBản { get; set; }
+        public bool KíchHoạt { get; set; }
+        public DateTime TạoLúc { get; set; }
+        public DateTime SửaLúc { get; set; }
+
+        // Thông tin mở rộng (khi join)
+        public string? TênPhim { get; set; }
+        public string? TênPhòng { get; set; }
+    }
+}
---
+++ BTL-API-quản lý rạp chiếu phim/QuanLyRapChieuPhim-Admin/QuanLyRapChieuPhim-Admin/code/DatabaseHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace QuanLyRapChieuPhim_Admin.code
+{
+    public static class DatabaseHelper
+    {
+        private static readonly string _connectionString =
+            "Data Source=WINDOWS-PC\\SQLEXPRESS;Initial Catalog=QuanLyRapPhim;Integrated Security=True;Trust Server Certificate=True";
+
+        public static SqlConnection GetConnection()
+        {
+            var conn = new SqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
